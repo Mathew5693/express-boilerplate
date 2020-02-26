f8bee08763b1f135593159a0805f8053fb206b38
Mathew Apanovich <Mapanovich@yahoo.com>
2020-02-26 10:02:00 -0500

    api token fix

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const {NODE_ENV} = require('./config')
+const {NODE_ENV, API_TOKEN} = require('./config')
 
 const app = express()
 
@@ -15,6 +15,19 @@ const morganOption = (NODE_ENV === 'production')
     app.use(helmet())
     app.use(cors())
 
+    /* Validate Token
+    app.use(function validateBearerToken(req, res, next){
+        const apiToken = API_TOKEN
+        const authToken = req.get('Authorization')
+        
+
+        if(!authToken || authToken.split(' ')[1] !== apiToken){
+            logger.error(`Unauthorized request to path: ${req.path}`)
+            return res.status(401).json({ error: 'Unauthorized request'})
+        }
+        next()
+    })
+    */
 
 
     app.get('/', (req, res) => {
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || 'ff80fb40-1c10-484e-b937-7d55c64c8866'
 }
